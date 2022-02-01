aca76e04e546949f234acc1bc8643256a32f1958
smh1988-2 <seamhurley1988@gmail.com>
2022-01-31 22:26:21 -0800

    new routes for podcasts

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,2 +1,34 @@
 class PodcastsController < ApplicationController
+
+    before_action :authorized, only: [:updated]
+
+    def create
+        podcast = Podcast.find_by_collectionId(params[:collectionId])
+        if podcast
+            render json: {"message" => "That podcast exists"}
+        else
+            new_podcast = Podcast.create!(podcast_params)
+            render json: new_podcast, status: :created
+        end
+    end
+
+    def index
+        podcasts = Podcast.all
+        render json: podcasts, status: :ok
+    end
+
+    def show
+        podcast = Podcast.find_by_collectionId(params[:id])
+        render json: podcast, status: :ok
+    end
+
+    def updated
+    end
+
+    private
+
+    def podcast_params
+        params.permit(:collectionName, :artistName, :artworkUrl100, :artworkUrl600, :primaryGenreName, :artistId, :collectionId)
+    end
+
 end
--- app/controllers/user_subscriptions_controller.rb
+++ app/controllers/user_subscriptions_controller.rb
@@ -1,6 +1,6 @@
 class UserSubscriptionsController < ApplicationController
 
-    before_action :authorized
+    before_action :authorized, only: [:updated]
 
     def create
         subscription = UserSubscription.find_by(user_id: params[:user_id], podcast_id: params[:podcast_id])
@@ -8,8 +8,12 @@ class UserSubscriptionsController < ApplicationController
             render json: {"Message: " => "You've already subscribed to this podcast!"}, status: :unprocessable_entity
         else
             new_subscription = UserSubscription.create!(subscription_params)
+            # create new activity too
             render json: new_subscription, status: :created
         end
+    end
+
+    def updated
     end
 
     private
