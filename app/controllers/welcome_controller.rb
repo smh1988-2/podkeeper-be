6e5c89f55c533f1ed37352fa50fd33c58d6f2d71
smh1988-2 <seamhurley1988@gmail.com>
2022-02-04 16:37:09 -0800

    deploy

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
 
+    before_action :authorized, only: [:updated_at] 
+
     def index
         render json: {"message: "=> "I'M ON THE INTERNET."}
+    end
+
+    def updated_at
     end
 end
