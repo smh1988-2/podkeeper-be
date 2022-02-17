c34b0c8bd380ee3afecf36192fd157feedad03cf
smh1988-2 <seamhurley1988@gmail.com>
2022-02-16 19:07:00 -0800

    fixing endpoint urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
 
-    resource "*",
+    resource "http://localhost:3000", "http://www.podkeeper.live", "https://podkeeper-fe.herokuapp.com/"
       # :expose => ['item_count'],
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
